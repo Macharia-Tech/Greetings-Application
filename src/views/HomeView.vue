aad3fee796b069b698e1a838f51b1509830f9d16
Audrey Macharia <mitchelaudrey@gmail.com>
2022-08-05 13:34:33 +0300

    added form and input where names are keyed in plus the output

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,29 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const greeting = ref('Hello " "')
+const greeting = ref(" ")
 
+// function onInput(e){
+//   greeting.value = e.target.value
+// }
 </script>
 
 <template>
 <div class="bg-img">
   <h1>Greetings Application </h1>
-  <form action="/dataCollectionLocation" method="post" autocomplete="on">
-  <button type="button">Cancel</button>
-  <button type="submit">Submit</button>
+    <div class="container">
+      <input v-model="greeting" placeholder="Your name">
+      <form action="/dataCollectionLocation" method="post" autocomplete="on">
+      <button type="button">Cancel</button>
+      <button type="submit">Submit</button>
 
   <!-- <input type="button" value="Cancel" />
   <input type="Submit" value="Submit" />
  -->
 
 </form>
+<p> Hello {{ greeting }}</p>
+</div>
 </div>
 </template>
 
@@ -39,11 +46,21 @@ button{
 h1{
   text-align: center;
   /* font-size:larger; */
-  text-shadow: 0.7cm;
+  /* text-shadow: 0.7cm; */
   /* font-family: 'Bungee Shade', cursive; */
+  font-family: 'Monoton', cursive;
   font-family: 'Diplomata', cursive;
   /* font-family: 'Ubuntu', sans-serif; */
-
-
+}
+.container{
+  position: absolute;
+  margin: 20px;
+  width: auto;
+  align-content: center;
+}
+p{
+  color:rgb(2, 1, 12);
+  font-size:50px;
+  font-style:bold;
 }
 </style>
